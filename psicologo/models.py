fce7e63decabcf55985930e94dc8c8234cadec65
LuisaFuentesL <l.fuentesl@uniandes.edu.co>
2022-03-27 19:03:11 -0500

    correccion length

--- psicologo/models.py
+++ psicologo/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Psicologo (models.Model):
-    nombre= models.CharField(max_lenght=50)
+    nombre= models.CharField(max_length=50)
     edad= models.FloatField(null=True, blank=True, default=None)
 
     def __str__(self):
