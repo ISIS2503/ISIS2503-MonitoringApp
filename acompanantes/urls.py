0a538028af0cbb437ec1ea67c9aac2acb6c670cd
JuanCamiloEduardo <69642359+JuanCamiloEduardo@users.noreply.github.com>
2022-03-29 11:54:33 -0500

    Update urls.py

--- acompanantes/urls.py
+++ acompanantes/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 from . import views
+
+urlpatterns = [
+    path('acompanantes/', views.acompanante_list),
+    path('acompanantecreate/', csrf_exempt(views.acompanante_create), name='acompananteCreate'),
+]
 """
