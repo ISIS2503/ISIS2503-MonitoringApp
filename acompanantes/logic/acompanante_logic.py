3688965b843f7488cb87055305ee1178127f00bb
JuanCamiloEduardo <69642359+JuanCamiloEduardo@users.noreply.github.com>
2022-03-29 11:51:53 -0500

    Update acompanante_logic.py

--- acompanantes/logic/acompanante_logic.py
+++ acompanantes/logic/acompanante_logic.py
@@ -1,9 +1,6 @@
 """
 from ..models import Acompanante
 
-def get_measurements():
-    queryset = Measurement.objects.all().order_by('-dateTime')[:10]
-    return (queryset)
 
 def create_acompanante(form):
     acompanante = form.save()
