4482deafc9c1f4ed2a0f12aa79d1f65bed7fbebb
j.burbanon <j.burbanon@uniandes.edu.co>
2022-03-29 23:55:45 -0500

    Comentado

--- acompanantes/urls.py
+++ acompanantes/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-
+"""
 from . import views
 
 urlpatterns = [
     path('acompanantes/', views.acompanante_list),
     path('acompanantecreate/', csrf_exempt(views.acompanante_create), name='acompananteCreate'),
 ]
-
+"""
