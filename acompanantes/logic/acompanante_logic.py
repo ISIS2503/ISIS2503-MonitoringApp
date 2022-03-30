7697b44ffb7af0e3ca086434380f769e8d91852a
j.burbanon <j.burbanon@uniandes.edu.co>
2022-03-29 23:33:53 -0500

    Hola

--- acompanantes/logic/acompanante_logic.py
+++ acompanantes/logic/acompanante_logic.py
@@ -1,6 +1,9 @@
 """
 from ..models import Acompanante
 
+def get_acompanante(var):
+    measurement = Measurement.objects.get(pk=var_pk)
+    return measurement
 
 def create_acompanante(form):
     acompanante = form.save()
