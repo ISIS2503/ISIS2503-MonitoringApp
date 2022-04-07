6dc7542e21f727f7eacfe10ce56d2b1879347c2d
LuisaFuentesL <l.fuentesl@uniandes.edu.co>
2022-04-06 19:10:23 -0500

    gets psicologo arreglados

--- psicologo/logic/psicologo_logic.py
+++ psicologo/logic/psicologo_logic.py
@@ -3,11 +3,11 @@
 
 from ..models import Psicologo
 
-def get_variables():
+def get_psicologo():
     queryset = Psicologo.objects.all()
     return (queryset)
 
-def create_cita(form):
+def create_psicologo(form):
     cita = form.save()
     cita.save()
     return ()
