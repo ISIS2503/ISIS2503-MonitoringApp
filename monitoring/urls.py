bc2bf0fbaf7b89bfbb5733760ef1f9101c82380e
afrodriguezm1 <af.rodriguezm1@uniandes.edu.co>
2023-03-22 12:02:19 -0500

    Fix healthcheck

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('health/', views.health, name='health'),
+    path('health/', views.health_check, name='health'),
     path('', views.index),
     path('', include('measurements.urls')),
     path('', include('variables.urls')),
