639a0e98a76a59a5943925e1648d48386b9286fa
PapiGelvez <s.gelvez@uniandes.edu.co>
2024-04-11 21:59:30 -0500

    default mail value

--- variables/models.py
+++ variables/models.py
@@ -6,7 +6,7 @@ class Variable(models.Model):
     country = models.CharField(max_length=50, default='Colombia')
     city = models.CharField(max_length=50, default='Bogota')
     phone = models.IntegerField()
-    mail = models.CharField(max_length=50)
+    mail = models.CharField(max_length=50, default='@bancodelosalpes.com.co')
 
     def __str__(self):
         return '{}'.format(self.name)
