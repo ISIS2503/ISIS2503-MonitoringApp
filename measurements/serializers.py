4df541020a04316f856cf8e07bb19b7f2ee12abc
fpungo <60239261+fpungo@users.noreply.github.com>
2024-04-13 03:12:07 -0500

    prueba

--- measurements/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-
-class MeasurementSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = ('id', 'trabajo', 'ingresos', 'deudas', 'creditos',)
-        model = models.Measurement
