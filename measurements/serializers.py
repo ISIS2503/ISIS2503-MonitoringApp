0d6ae24faeff3e63fd435110362a1ba62cdbd120
Laura <lv.larad@uniandes.edu.co>
2024-10-05 19:52:14 -0500

    sistema matriculas

--- measurements/serializers.py
+++ measurements/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
 from . import models
 
-
 class MeasurementSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = ('id', 'variable', 'value', 'unit', 'place', 'time',)
+        fields = ('id', 'variable', 'value', 'unit', 'place', 'dateTime',)  # Adaptado para matrícula
         model = models.Measurement
