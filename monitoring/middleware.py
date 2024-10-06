74eafbb2f01619c57c9e5f8dabc5bbeea6a0b45c
Laura <lv.larad@uniandes.edu.co>
2024-10-05 23:33:20 -0500

    aplicacion

--- monitoring/middleware.py
+++ monitoring/middleware.py
@@ -1,22 +1,25 @@
-# monitoring/middleware.py
-
-import time
 import logging
+import time
 
-logger = logging.getLogger('request_timing')
+logger = logging.getLogger(__name__)
 
 class RequestTimingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        logger.info("Request Timing Middleware initialized.")  # Agrega un mensaje de prueba aquí
 
     def __call__(self, request):
         start_time = time.time()
-        
         response = self.get_response(request)
-        
         end_time = time.time()
-        duration = end_time - start_time
-        logger.info(f"Request to {request.path} took {duration:.2f} seconds")  # Este debe ir al log
+
+        processing_time = end_time - start_time
         
+        # Medir el tiempo de registro
+        log_start_time = time.time()
+        logger.info(f"Request to {request.path} took {processing_time:.2f} seconds")
+        log_end_time = time.time()
+
+        log_time = log_end_time - log_start_time
+        logger.info(f"Logging took {log_time:.6f} seconds")  # Cambié a 6 decimales para mayor precisión
+
         return response
