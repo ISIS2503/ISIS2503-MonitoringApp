c347bb3a9cb1db6cf48a9a397caa5bb5dfa7de90
Juanin6 <jn.suarez@uniandes.edu.co>
2024-10-09 00:58:41 -0500

    Cambios saldos Usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+import random
 class Usuario(models.Model):
     nombre = models.CharField(max_length=50)
     edad = models.IntegerField()
     correo = models.CharField(max_length=50)
     contrasena = models.CharField(max_length=50)
+    saldoTotal = random.randint(100000,1000000)
+    saldoDeber = random.randint(100000,1000000)
 
     def __str__(self):
         return '{} ({})'.format(self.nombre, self.edad)
