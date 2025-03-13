abbbf0055b6e8d0ca9d2c8a861ea3b6be561e8a9
Daniel Bolivar <5009484+danielbolivar@users.noreply.github.com>
2025-03-13 03:17:27 -0500

    Remove unnecessary installation and Cloud SQL Proxy setup steps from migration script

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -6,22 +6,6 @@ PROJECT_ID="arquisoft-453601"
 REGION="us-central1"
 INSTANCE_NAME="django-db-instance"
 
-echo "Instalando dependencias para migraciones..."
-pip install psycopg2-binary google-cloud-secret-manager django
-
-echo "Configurando Cloud SQL Proxy..."
-# Descargar el proxy
-wget https://dl.google.com/cloudsql/cloud-sql-proxy.linux.amd64 -O cloud-sql-proxy
-chmod +x cloud-sql-proxy
-
-# Iniciar el proxy en segundo plano
-./cloud-sql-proxy "${PROJECT_ID}:${REGION}:${INSTANCE_NAME}" &
-PROXY_PID=$!
-
-# Esperar a que el proxy se inicie
-echo "Esperando a que el proxy se inicie..."
-sleep 5
-
 # Realizar las migraciones
 echo "Ejecutando makemigrations..."
 DJANGO_SETTINGS_MODULE=monitoring.settings_prod python manage.py makemigrations
