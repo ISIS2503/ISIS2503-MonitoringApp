ed39625b9dd1b04dd911c13eaefc7a1167058e58
Juan Andrés Avelino Ospina <ja.avelino@uniandes.edu.co>
2025-10-15 12:18:30 -0500

    Enable public IP address for database instance

--- circuit-breaker/deployment.tf
+++ circuit-breaker/deployment.tf
@@ -174,7 +174,7 @@ resource "aws_instance" "kong" {
 resource "aws_instance" "database" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
-  associate_public_ip_address = false
+  associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.traffic_db.id, aws_security_group.traffic_ssh.id]
 
   user_data = <<-EOT
